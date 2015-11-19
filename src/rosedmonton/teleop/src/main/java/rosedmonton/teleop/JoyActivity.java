2e264d4a59b1a5583bff36fc9159ff63f6fdfc94
Diego Rodriguez <dh.rodriguez25@uniandes.edu.co>
2015-11-18 17:48:51 -0700

    Added Streaming

--- src/rosedmonton/teleop/src/main/java/rosedmonton/teleop/JoyActivity.java
+++ src/rosedmonton/teleop/src/main/java/rosedmonton/teleop/JoyActivity.java
@@ -1,15 +1,22 @@
 package rosedmonton.teleop;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import org.ros.address.InetAddressFactory;
+import org.ros.android.BitmapFromCompressedImage;
 import org.ros.android.NodeMainExecutorService;
 import org.ros.android.RosActivity;
+import org.ros.android.view.RosImageView;
 import org.ros.android.view.VirtualJoystickView;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
 
 import java.net.URI;
+
+import sensor_msgs.CompressedImage;
 
 /**
  * Created by dhrodriguezg on 11/12/15.
@@ -18,6 +25,7 @@ public class JoyActivity extends RosActivity {
 
     private static final String TAG = "JoyActivity";
     private VirtualJoystickView joystick;
+    private RosImageView<CompressedImage> imageStreamNodeMain;
     private NodeMainExecutorService nodeMain;
 
     public JoyActivity() {
@@ -27,11 +35,20 @@ public class JoyActivity extends RosActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.joystick_teleop);
 
         joystick = (VirtualJoystickView) findViewById(R.id.virtual_joystick);
         joystick.setHolonomic(false);
         joystick.setTopicName("/android/joystick");
+
+        imageStreamNodeMain = (RosImageView<CompressedImage>) findViewById(R.id.streamingView);
+        imageStreamNodeMain.setMessageToBitmapCallable(new BitmapFromCompressedImage());
+        imageStreamNodeMain.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imageStreamNodeMain.setTopicName("/camera/image/compressed");
+        imageStreamNodeMain.setMessageType("sensor_msgs/CompressedImage");
     }
 
     @Override
@@ -45,5 +62,6 @@ public class JoyActivity extends RosActivity {
         nodeMain=(NodeMainExecutorService)nodeMainExecutor;
         NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress(), getMasterUri());
         nodeMainExecutor.execute(joystick, nodeConfiguration.setNodeName("/android/teleop_node"));
+        nodeMainExecutor.execute(imageStreamNodeMain, nodeConfiguration.setNodeName("/android/camera_receiver"));
     }
 }
